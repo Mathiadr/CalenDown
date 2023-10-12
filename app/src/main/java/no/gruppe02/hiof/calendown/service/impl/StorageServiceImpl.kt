c24f790e42ea7438829457efe902f66e69466979
Mathiadr <mathiadr@hiof.no>
2023-10-12 15:06:55 +0200

    Reverted and Fixed LOTS OF BUGS

--- app/src/main/java/no/gruppe02/hiof/calendown/service/impl/StorageServiceImpl.kt
+++ app/src/main/java/no/gruppe02/hiof/calendown/service/impl/StorageServiceImpl.kt
@@ -9,16 +9,14 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.tasks.await
 import no.gruppe02.hiof.calendown.model.Event
-import no.gruppe02.hiof.calendown.service.AccountService
-import no.gruppe02.hiof.calendown.service.StorageService
+
 import javax.inject.Inject
 
 class StorageServiceImpl
 @Inject
-constructor(private val firestore: FirebaseFirestore,
-            private val auth: AccountService
-) : StorageService {
-
+constructor(
+)  {
+    /*
     @OptIn(ExperimentalCoroutinesApi::class)
     override val events: Flow<List<Event>>
         get() = auth.currentUser.flatMapLatest { user ->
@@ -44,4 +42,6 @@ constructor(private val firestore: FirebaseFirestore,
         private const val EVENT_COLLECTION = "events"
         private const val USER_ID_FIELD = "userId"
     }
+
+     */
 }
--- app/src/main/java/no/gruppe02/hiof/calendown/service/module/ServiceModule.kt
+++ app/src/main/java/no/gruppe02/hiof/calendown/service/module/ServiceModule.kt
@@ -1,14 +1,7 @@
 package no.gruppe02.hiof.calendown.service.module
 
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import no.gruppe02.hiof.calendown.service.AccountService
-import no.gruppe02.hiof.calendown.service.StorageService
-import no.gruppe02.hiof.calendown.service.impl.AccountServiceImpl
-import no.larseknu.hiof.compose.playingwithfirebase.service.impl.StorageServiceImpl
 
+/*
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class ServiceModule {
@@ -17,4 +10,4 @@ abstract class ServiceModule {
 
     @Binds
     abstract fun provideAccountService(impl: AccountServiceImpl): AccountService
-}
+}*/
