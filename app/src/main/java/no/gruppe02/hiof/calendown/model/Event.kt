fca5d23c9b136cdc014ceba7242ded36e479e8d2
Mathiadr <mathiadr@hiof.no>
2023-10-12 11:43:53 +0200

    Added Event detail screens

--- app/src/main/java/no/gruppe02/hiof/calendown/model/Event.kt
+++ app/src/main/java/no/gruppe02/hiof/calendown/model/Event.kt
@@ -1,8 +1,16 @@
 package no.gruppe02.hiof.calendown.model
 
+import java.util.Date
+
 data class Event(
     val uid: String = "",
+    var owner: User,
+
     var title: String = "",
-    var description : String = "",
-    var image : String = "",
-    var icon : String = "")
+    var description: String = "",
+    var image: String = "",
+    var icon: String = "",
+    var category: String = "",
+
+    var date: Date,
+    )
