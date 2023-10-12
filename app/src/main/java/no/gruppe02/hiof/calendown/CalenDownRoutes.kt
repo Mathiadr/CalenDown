e26a8514aeb23ccf9cad23a13b2009ed3487048d
Mathiadr <mathiadr@hiof.no>
2023-10-12 18:48:21 +0200

    Implementing FirebaseModule

--- app/src/main/java/no/gruppe02/hiof/calendown/CalenDownRoutes.kt
+++ app/src/main/java/no/gruppe02/hiof/calendown/CalenDownRoutes.kt
@@ -1,4 +1,10 @@
 package no.gruppe02.hiof.calendown
 
-const val EVENT_ID = "movieId"
+
+const val EVENT_LIST = "eventList"
+const val EVENT_DETAIL = "eventDetail"
+
+
+
+const val EVENT_ID = "eventId"
 const val EVENT_ID_ARG = "?$EVENT_ID={$EVENT_ID}"
--- app/src/main/java/no/gruppe02/hiof/calendown/service/module/ServiceModule.kt
+++ app/src/main/java/no/gruppe02/hiof/calendown/service/module/ServiceModule.kt
@@ -1,13 +1,24 @@
 package no.gruppe02.hiof.calendown.service.module
 
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import no.gruppe02.hiof.calendown.service.AccountService
+import no.gruppe02.hiof.calendown.service.StorageService
+import no.gruppe02.hiof.calendown.service.impl.AccountServiceImpl
+import no.gruppe02.hiof.calendown.service.impl.StorageServiceImpl
 
-/*
+
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class ServiceModule {
     @Binds
     abstract fun provideStorageService(impl: StorageServiceImpl): StorageService
 
+    /*
     @Binds
     abstract fun provideAccountService(impl: AccountServiceImpl): AccountService
-}*/
+
+     */
+}
