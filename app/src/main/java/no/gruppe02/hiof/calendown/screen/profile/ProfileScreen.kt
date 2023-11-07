90eeafff9697e262b614544dc9323c816a9cf01d
Oliver Kimmell <oliverdk@hiof.no>
2023-11-07 10:08:12 +0100

    Started on profilescreen. Added dummy circle img

--- app/src/main/java/no/gruppe02/hiof/calendown/screen/profile/ProfileScreen.kt
+++ app/src/main/java/no/gruppe02/hiof/calendown/screen/profile/ProfileScreen.kt
@@ -1,15 +1,88 @@
 package no.gruppe02.hiof.calendown.screen.profile
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import no.gruppe02.hiof.calendown.R
 
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(
     modifier: Modifier = Modifier,
     viewModel: ProfileViewModel = hiltViewModel()
 ) {
-    Text(text = "Logged in as " + viewModel.userId)
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Profile",
+                        style = MaterialTheme.typography.displaySmall,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
+                )
+            )
+        }
+    ) { innerPadding -> Column (
+        modifier = Modifier
+            .padding(innerPadding)
+            .padding(28.dp)){
+        Row {
+            RoundImage(
+                image = painterResource(R.drawable.profilepic),
+                modifier = Modifier
+                    .size(100.dp)
+            )
+        }
+        Text(text = "Logged in as " + viewModel.userId)
+    }
+    }
 }
+
+@Composable
+fun RoundImage (
+    image: Painter,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = modifier
+            .aspectRatio(1f, matchHeightConstraintsFirst = true)
+            .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = CircleShape
+            )
+            .padding(3.dp)
+            .clip(CircleShape))
+}
+
